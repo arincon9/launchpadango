a8141b02615580c0658e919d5e562290a036b5e3
Alex Rincon <alejandro.rincon9@gmail.com>
2016-03-28 19:22:34 -0700

    Add Movie model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329013227) do
+ActiveRecord::Schema.define(version: 20160329021036) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "runtime"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "theatres", force: :cascade do |t|
     t.integer  "capacity"
