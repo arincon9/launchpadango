28afe63849973c1a0da9e38227f51eee9d3220fe
Alex Rincon <alejandro.rincon9@gmail.com>
2016-03-30 10:17:37 -0700

    Add routes for showtimes and orders.
    Add nested resources of showtimes to movies.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get 'showtimes/index'
+
+  get 'showtimes/new'
+
+  get 'orders/index'
+
+  get 'orders/new'
+
   root "movies#index"
 
   resources :theatres
-  resources :movies
+  resources :movies do
+    resources :showtimes
+  end
+  resources :orders
+  resources :showtimes
 end
