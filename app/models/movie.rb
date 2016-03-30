e8d7090f157958839369a447dd7670cc84d624e5
Alex Rincon <alejandro.rincon9@gmail.com>
2016-03-30 10:07:11 -0700

    Refactor the inverse of movies and add additional validations.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,9 @@
 class Movie < ActiveRecord::Base
-  has_many :showtimes, dependent: :destroy, inverse_of: :showtime
+  has_many :showtimes, dependent: :destroy, inverse_of: :movie
+  has_many :orders, inverse_of: :movie
 
-  validates :title, presence: true
-
-  validates :description, presence: true
+  validates :title,       presence:     true
+  validates :description, presence:     true
+  validates :runtime,     presence:     true,
+                          numericality: { greater_than: 0}
 end
