3e40be81929323bd9014d717c8f3493e82c7592e
Alex Rincon <alejandro.rincon9@gmail.com>
2016-03-30 10:14:12 -0700

    Add additional validations to the theatre model.

--- app/models/theatre.rb
+++ app/models/theatre.rb
@@ -1,8 +1,10 @@
 class Theatre < ActiveRecord::Base
-  has_many :showtimes, dependent: :destroy, inverse_of: :showtime
+  has_many :showtimes, dependent: :destroy, inverse_of: :theatre
 
   validates :number, uniqueness: true,
-                     presence:   true
+                     presence:   true,
+                     numericality: { greater_than: 0 }
 
-  validates :capacity, presence: true
+  validates :capacity, presence: true,
+                       numericality: { greater_than: 0 }
 end
