c3f7c4b44bd2bed7ff9d072bbd0c1915e18c9d17
Alex Rincon <alejandro.rincon9@gmail.com>
2016-03-30 10:13:41 -0700

    Add a has_many relationship to orders.

--- app/models/showtime.rb
+++ app/models/showtime.rb
@@ -1,6 +1,7 @@
 class Showtime < ActiveRecord::Base
   belongs_to :movie, inverse_of: :showtimes
   belongs_to :theatre, inverse_of: :showtimes
+  has_many :orders, inverse_of: :showtime
 
   validates :time, presence:   true
 end
