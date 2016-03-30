7a644b97c2b93ae8b666894c815268b3b5551bcc
Alex Rincon <alejandro.rincon9@gmail.com>
2016-03-29 18:00:30 -0700

    Remove numericality validations on integers.

--- app/models/theatre.rb
+++ app/models/theatre.rb
@@ -1,10 +1,8 @@
 class Theatre < ActiveRecord::Base
   has_many :showtimes, dependent: :destroy, inverse_of: :showtime
 
-  validates :number, uniqueness:   true,
-                     numericality: { only_integer: true },
-                     presence:     true
+  validates :number, uniqueness: true,
+                     presence:   true
 
-  validates :capacity, numericality: { only_integer: true },
-                       presence:     true
+  validates :capacity, presence: true
 end
