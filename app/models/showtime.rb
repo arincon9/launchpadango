b455abdc67d43e74cc8feabcdf041d8fdbe24243
Alex Rincon <alejandro.rincon9@gmail.com>
2016-03-31 08:50:16 -0700

    Limit order form to a specific movie.

--- app/models/showtime.rb
+++ app/models/showtime.rb
@@ -3,5 +3,18 @@ class Showtime < ActiveRecord::Base
   belongs_to :theatre, inverse_of: :showtimes
   has_many :orders, inverse_of: :showtime
 
-  validates :time, presence:   true
+  validates :time, presence: true
+
+  validate :ensure_theatre_uniqueness
+
+  scope :upcoming, -> { where(<<-SQL) }
+    time > '#{Time.now}'
+    AND time < '#{Time.now + 2.weeks }'
+  SQL
+
+  scope :for_movie, ->(movie_id) { where(movie_id: movie_id) }
+
+  def display_name
+    time.strftime("%A, %b %d at %H:%M")
+  end
 end
