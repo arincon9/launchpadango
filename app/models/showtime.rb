3f82b9d4bf0007d1435a0484fb4bf96c461e29c2
Alex Rincon <alejandro.rincon9@gmail.com>
2016-03-31 09:27:21 -0700

    Use 12 hour display time and add meridian indicator.

--- app/models/showtime.rb
+++ app/models/showtime.rb
@@ -5,8 +5,6 @@ class Showtime < ActiveRecord::Base
 
   validates :time, presence: true
 
-  validate :ensure_theatre_uniqueness
-
   scope :upcoming, -> { where(<<-SQL) }
     time > '#{Time.now}'
     AND time < '#{Time.now + 2.weeks }'
@@ -15,6 +13,6 @@ class Showtime < ActiveRecord::Base
   scope :for_movie, ->(movie_id) { where(movie_id: movie_id) }
 
   def display_name
-    time.strftime("%A, %b %d at %H:%M")
+    time.strftime("%A, %b %d at %I:%M %p")
   end
 end
