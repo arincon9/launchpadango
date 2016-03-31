b5a0812934913b02258560df93417fba4ea7db59
Alex Rincon <alejandro.rincon9@gmail.com>
2016-03-31 07:48:19 -0700

    Add support for the order purchase amount and for matinee pricing.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ActiveRecord::Base
   belongs_to :movie, inverse_of: :orders
   belongs_to :showtime, inverse_of: :orders
+  belongs_to :theatre, inverse_of: :orders
 
   validates :customer_name, presence: true
   validates :customer_email, presence: true,
@@ -15,4 +16,12 @@ class Order < ActiveRecord::Base
   validates :billing_address_1, presence: true
   validates :billing_zipcode, presence: true,
                               numericality: { greater_than: 0 }
+
+  def purchase_amount
+    if showtime.time.hour < 4
+      "$11.00"
+    else
+      "$15.00"
+    end
+  end
 end
