b765bfa3629b7f0e3298ecb38bd9e6a8f2d95b42
Alex Rincon <alejandro.rincon9@gmail.com>
2016-03-31 20:52:49 -0700

    Add orders association to Theatre and add display name method to render number and capacity together.

--- app/models/theatre.rb
+++ app/models/theatre.rb
@@ -1,5 +1,6 @@
 class Theatre < ActiveRecord::Base
   has_many :showtimes, dependent: :destroy, inverse_of: :theatre
+  has_many :orders, inverse_of: :theatre
 
   validates :number, uniqueness: true,
                      presence:   true,
@@ -7,4 +8,8 @@ class Theatre < ActiveRecord::Base
 
   validates :capacity, presence: true,
                        numericality: { greater_than: 0 }
+
+  def display_name
+    "Theatre: #{number} - capacity: #{capacity}"
+  end
 end
